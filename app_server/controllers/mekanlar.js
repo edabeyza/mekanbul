7327ddf442c160a071ca80f133e2402ce396f517
edabeyza <edabeyzaozeren@gmail.com>
2022-10-24 01:10:02 +0300

    mekanbul

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,7 +1,5 @@
 var express = require("express");
 var router = express.Router();
-
-// slash gördüğün zaman function çalışır
 const anaSayfa = function (req, res, next) {
   res.render("anasayfa", {
     "baslik": "Anasayfa",
@@ -9,74 +7,68 @@ const anaSayfa = function (req, res, next) {
       "siteAd": "Mekanbul",
       "slogan": "Civardaki Mekanları Keşfet!",
     },
-    "mekanlar": [{
-        "ad": "Barida Kafe",
-        "adres": "Sdü Batı Kampüsü",
-        "puan": "4",
-        "mesafe": "2km",
-        "imkanlar": ["Kahve", "Çay", "çikolata"]
+    "mekanlar":[
+      {
+        "ad":"Barida Kafe",
+        "adres":"Sdü Batı Kampüsü",
+        "puan":"4",
+        "mesafe":"2km",
+        "imkanlar":["Kahve","Çay","çikolata"]
       },
       {
-        "ad": "Gloria",
-        "adres": "Sdü Doğu Kampüsü",
-        "puan": "2",
-        "mesafe": "5km",
-        "imkanlar": ["Gazoz", "Tavuk döner", "Poğaca"]
-      }
-    ]
+        "ad":"Gloria",
+        "adres":"Sdü Doğu Kampüsü",
+        "puan":"2",
+        "mesafe":"5km",
+        "imkanlar":["Gazoz","Tavuk döner","Poğaca"]
+      }]
   });
 };
 
 const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi", {
-    "baslik": "Mekan Bilgisi",
-    "mekanBaslik": "Barida Kafe",
-    "mekanDetay": {
-      "ad": "Barida Kafe",
-      "adres": "Sdü Batı Kampüsü",
-      "puan": "4",
-      "saatler": [{
-          "gunler": "  Pazartesi-Cuma",
-          "acilis": "9:00",
-          "kapanis": "23:00",
+  res.render("mekanbilgisi",
+  { "baslik": "Mekan Bilgisi",
+   "mekanBaslik":"Barida Kafe",
+   "mekanDetay":{
+      "ad":"Barida Kafe",
+      "adres":"Sdü Batı Kampüsü",
+      "puan":"4",
+      "saatler":[
+        {
+          "gunler":"  Pazartesi-Cuma",
+          "acilis":"9:00",
+          "kapanis":"23:00",
           "kapali": false
         },
         {
-          "gunler": "  Cumartesi-Pazar",
-          "acilis": "10:00",
-          "kapanis": "22:00",
+          "gunler":"  Cumartesi-Pazar",
+          "acilis":"10:00",
+          "kapanis":"22:00",
           "kapali": false
-        }
-      ],
-      "imkanlar": ["Kahve", "Çay", "çikolata"],
-      "koordinatlar": {
-        "enlem": "37.7",
-        "boylam": "30.5"
+        }],
+      "imkanlar":["Kahve","Çay","çikolata"],
+      "koordinatlar":{
+        "enlem":"37.7",
+        "boylam":"30.5"
       },
-      "yorumlar": [{
-          "yorumYapan": "Beyza",
-          "puan": "3",
-          "tarih": "20 Ekim 2022",
-          "yorumMetni": "rezilllll"
+      "yorumlar":[
+        {
+          "yorumYapan":"Emre",
+          "puan":"1",
+          "tarih":"19 Aralık 2021",
+          "yorumMetni":"Hiç beğenmedim. iğrenç."
         },
         {
-          "yorumYapan": "Ahmet",
-          "puan": "2",
-          "tarih": "20 Ekim 2022",
-          "yorumMetni": "kötü"
-        }
-      ]
-    }
-  });
+          "yorumYapan":"Bahar",
+          "puan":"5",
+          "tarih":"23 Nisan 2020",
+          "yorumMetni":"Yemekler lezizdi!"
+        }]
+   }
+});
 };
 
 const yorumEkle = function (req, res, next) {
-  res.render("yorumekle", {
-    title: "Yorum Sayfası"
-  });
+  res.render("yorumekle", { title: "Yorum Sayfası" });
 };
-module.exports = {
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle
-};
+module.exports = { anaSayfa, mekanBilgisi, yorumEkle };
