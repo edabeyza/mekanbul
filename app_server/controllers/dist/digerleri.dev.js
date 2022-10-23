28707b65d00dfb14522c0695287a7643aee8ab38
edabeyza <edabeyzaozeren@gmail.com>
2022-10-23 23:10:47 +0300

    mekanbul

---
+++ app_server/controllers/dist/digerleri.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var hakkinda = function hakkinda(req, res, next) {
+  res.render('hakkinda', {
+    title: 'Hakkinda'
+  });
+};
+
+module.exports = {
+  hakkinda: hakkinda
+};
+//# sourceMappingURL=digerleri.dev.js.map
