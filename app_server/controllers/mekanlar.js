b5c27d5af224dc8267d6e9a894f292854bce08a3
rosecoder0 <89976307+rosecoder0@users.noreply.github.com>
2022-10-23 23:58:11 +0300

    mekanbul

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,23 +1,74 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
 const anaSayfa = function (req, res, next) {
-  res.render('anasayfa', { title: 'Ana Sayfa' });
-}
+  res.render("anasayfa", {
+    "baslik": "Anasayfa",
+    "sayfaBaslik": {
+      "siteAd": "Mekanbul",
+      "slogan": "Civardaki Mekanları Keşfet!",
+    },
+    "mekanlar":[
+      {
+        "ad":"Barida Kafe",
+        "adres":"Sdü Batı Kampüsü",
+        "puan":"4",
+        "mesafe":"2km",
+        "imkanlar":["Kahve","Çay","çikolata"]
+      },
+      {
+        "ad":"Gloria",
+        "adres":"Sdü Doğu Kampüsü",
+        "puan":"2",
+        "mesafe":"5km",
+        "imkanlar":["Gazoz","Tavuk döner","Poğaca"]
+      }]
+  });
+};
 
 const mekanBilgisi = function (req, res, next) {
-  res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
-}
-
+  res.render("mekanbilgisi",
+  { "baslik": "Mekan Bilgisi",
+   "mekanBaslik":"Barida Kafe",
+   "mekanDetay":{
+      "ad":"Barida Kafe",
+      "adres":"Sdü Batı Kampüsü",
+      "puan":"4",
+      "saatler":[
+        {
+          "gunler":"  Pazartesi-Cuma",
+          "acilis":"9:00",
+          "kapanis":"23:00",
+          "kapali": false
+        },
+        {
+          "gunler":"  Cumartesi-Pazar",
+          "acilis":"10:00",
+          "kapanis":"22:00",
+          "kapali": false
+        }],
+      "imkanlar":["Kahve","Çay","çikolata"],
+      "koordinatlar":{
+        "enlem":"37.7",
+        "boylam":"30.5"
+      },
+      "yorumlar":[
+        {
+          "yorumYapan":"Emre",
+          "puan":"1",
+          "tarih":"19 Aralık 2021",
+          "yorumMetni":"Hiç beğenmedim."
+        },
+        {
+          "yorumYapan":"Bahar",
+          "puan":"5",
+          "tarih":"23 Nisan 2020",
+          "yorumMetni":"Yemekler lezizdi!"
+        }]
+   }
+});
+};
 
 const yorumEkle = function (req, res, next) {
-  res.render('yorumekle', { title: 'Yorum Ekle' });
-}
-
-module.exports = {
-
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle 
-
-}
+  res.render("yorumekle", { title: "Yorum Sayfası" });
+};
+module.exports = { anaSayfa, mekanBilgisi, yorumEkle };
