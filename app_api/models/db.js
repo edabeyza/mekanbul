257201a4fd80e0f0eb9fbd3609517b91f4643f42
Beyza Özeren <72518776+edabeyza@users.noreply.github.com>
2022-12-03 18:02:43 +0300

    odev7

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose=require("mongoose");
 require("./mekansema");
-var dbURI = 'mongodb://localhost/mekanbul'; 
-//var dbURI="mongodb+srv://edabeyza:1234@mekanbul.afqhtug.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
+//var dbURI = 'mongodb://localhost/mekanbul'; 
+var dbURI="mongodb+srv://edabeyza:1234@mekanbul.afqhtug.mongodb.net/mekanbul?retryWrites=true&w=majority"; 
 mongoose.connect(dbURI);
 
 function kapat(msg,callback){
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,74 +1,105 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const anaSayfa = function (req, res, next) {
-  res.render("anasayfa", {
-    "baslik": "Anasayfa",
-    "sayfaBaslik": {
-      "siteAd": "Mekanbul",
-      "slogan": "Civardaki Mekanları Keşfet!",
-    },
-    "mekanlar":[
-      {
-        "ad":"Barida Kafe",
-        "adres":"Sdü Batı Kampüsü",
-        "puan":"4",
-        "mesafe":"2km",
-        "imkanlar":["Kahve","Çay","çikolata"]
-      },
-      {
-        "ad":"Gloria",
-        "adres":"Sdü Doğu Kampüsü",
-        "puan":"2",
-        "mesafe":"5km",
-        "imkanlar":["Gazoz","Tavuk döner","Poğaca"]
-      }]
-  });
+const axios = require('axios');
+
+var apiSecenekleri = {
+    sunucu: "http://localhost:3000",
+    //sunucu:"",
+    apiYolu: "/api/mekanlar/",
 };
 
-const mekanBilgisi = function (req, res, next) {
-  res.render("mekanbilgisi",
-  { "baslik": "Mekan Bilgisi",
-   "mekanBaslik":"Barida Kafe",
-   "mekanDetay":{
-      "ad":"Barida Kafe",
-      "adres":"Sdü Batı Kampüsü",
-      "puan":"4",
-      "saatler":[
-        {
-          "gunler":"  Pazartesi-Cuma",
-          "acilis":"9:00",
-          "kapanis":"23:00",
-          "kapali": false
-        },
-        {
-          "gunler":"  Cumartesi-Pazar",
-          "acilis":"10:00",
-          "kapanis":"22:00",
-          "kapali": false
-        }],
-      "imkanlar":["Kahve","Çay","çikolata"],
-      "koordinatlar":{
-        "enlem":"37.7",
-        "boylam":"30.5"
-      },
-      "yorumlar":[
-        {
-          "yorumYapan":"Emre",
-          "puan":"1",
-          "tarih":"19 Aralık 2021",
-          "yorumMetni":"Hiç beğenmedim."
-        },
-        {
-          "yorumYapan":"Bahar",
-          "puan":"5",
-          "tarih":"23 Nisan 2020",
-          "yorumMetni":"Yemekler lezizdi!"
-        }]
-   }
-});
+var mesafeyiFormatla = function(mesafe) {
+    var yeniMesafe, birim;
+    if (mesafe > 1) {
+        yeniMesafe = parseFloat(mesafe).toFixed(1);
+        birim = " km";
+    } else {
+        yeniMesafe = parseInt(mesafe * 1000, 10);
+        birim = " m"
+    }
+    return yeniMesafe + birim;
 };
 
-const yorumEkle = function (req, res, next) {
-  res.render("yorumekle", { title: "Yorum Sayfası" });
+var anaSayfaOlustur = function(res, mekanListesi) {
+    var mesaj;
+    if (!(mekanListesi instanceof Array)) {
+        mesaj = "API HATASI: Birşeyler ters gitti!";
+        mekanListesi = [];
+    } else {
+        if (!mekanListesi.length) {
+            mesaj = "Civarda herhangi bir mekan bulunamadı!";
+        }
+    }
+    res.render("anasayfa", {
+        "baslik" : "Anasayfa",
+        "sayfaBaslik" : {
+            "siteAd" : "Mekanbul",
+            "slogan" : "Civardaki Mekanları Keşfet!"
+        },
+        "mekanlar" : mekanListesi,
+        "mesaj" : mesaj
+    });
 };
-module.exports = { anaSayfa, mekanBilgisi, yorumEkle };
+
+const anaSayfa = function(req, res, next) {
+    axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
+        params : {
+            enlem : req.query.enlem,
+            boylam : req.query.boylam
+        },
+    }).then(function(response) {
+        var i, mekanlar;
+        mekanlar = response.data;
+        for (i = 0; i < mekanlar.length; i++) {
+            mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
+        }
+        anaSayfaOlustur(res, mekanlar);
+    }).catch(function(hata) {
+        anaSayfaOlustur(res, hata);
+    });
+};
+
+var detaySayfasiOlustur = function(res, mekanDetaylari) {
+    mekanDetaylari.koordinat = {
+        "enlem" : mekanDetaylari.koordinat[0],
+        "boylam" : mekanDetaylari.koordinat[1]
+    }
+    res.render('mekanbilgisi', {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
+    });
+};
+
+var hataGoster = function(res, hata) {
+    var mesaj;
+    if (hata.response.status == 404) {
+        mesaj = "404, Sayfa bulunamadı!";
+    } else {
+        mesaj = hata.response.status + " hatası.";
+    }
+    res.status(hata.response.status);
+    res.render('error', {
+        "mesaj" : mesaj
+    });
+};
+
+const mekanBilgisi = function(req, res) {
+    axios
+    .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+    .then(function(response) {
+        detaySayfasiOlustur(res, response.data);
+    })
+    .catch(function(hata) {
+        hataGoster(res, hata);
+    });
+};
+
+const yorumEkle=function(req, res, next) {
+    res.render('yorumekle', { title: 'Yorum Ekle' });
+};
+
+module.exports={
+    anaSayfa,
+    mekanBilgisi,
+    yorumEkle
+}
